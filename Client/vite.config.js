7d1e16b8d54818c404e2c0b9909831711aa9bc53
Pawel Kluska <pkluska@griddynamics.com>
2024-03-24 16:07:00 +0100

    init project

---
+++ Client/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  host: true,
+  strictPort: true,
+  server: {
+    port: 80,
+  }
+})
