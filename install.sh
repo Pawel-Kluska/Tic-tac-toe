11a4672bfa8df40ba312331aa00b85da0fcf0203
Pawel Kluska <pkluska@griddynamics.com>
2024-04-20 16:20:41 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo su
 yum update -y
 yum install -y yum-utils device-mapper-persistent-data lvm2 python3-pip git docker
 
@@ -12,6 +12,8 @@ if [ ! -f "/usr/local/bin/docker-compose" ]; then
     wget -q https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -O /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
-echo "IP_ADDRESS=$(curl -s ifconfig.me | awk '{print $1}')" | sudo tee /home/ec2-user/Tic-tac-toe/.env > /dev/null
+curl checkip.amazonaws.com > /home/ec2-user/hehe.txt
+echo "IP_ADDRESS=$(curl checkip.amazonaws.com)" | sudo tee /home/ec2-user/Tic-tac-toe/.env > /dev/null
+export IP_ADDRESS=$(curl checkip.amazonaws.com)
 git clone https://github.com/Pawel-Kluska/Tic-tac-toe.git /home/ec2-user/Tic-tac-toe
 #sudo docker-compose -f /home/ec2-user/Tic-tac-toe/docker-compose-prod.yml up -d
