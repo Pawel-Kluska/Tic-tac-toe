54eb4e0c59e346165b6e1eba296d3cc21f86b302
Pawel Kluska <pkluska@griddynamics.com>
2024-04-20 12:43:01 +0200

    .

--- Client/src/constants/index.js
+++ Client/src/constants/index.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NODE_ENV === 'prod' ? process.env.PUBLIC_IP + ':8080' : 'http://localhost:8080';
+export const API_BASE_URL = process.env.NODE_ENV === 'prod' ? process.env.REACT_APP_PUBLIC_IP + ':8080' : 'http://localhost:8080';
