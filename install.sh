5d57316d5c185aadad8ab195368d18bbeb7d424f
Pawel Kluska <pkluska@griddynamics.com>
2024-04-20 11:22:46 +0200

    .

--- install.sh
+++ install.sh
@@ -12,10 +12,9 @@ if [ ! -f "/usr/local/bin/docker-compose" ]; then
     wget -q https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -O /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
-export PUBLIC_IP=$(curl -s ifconfig.me | awk '{print $1}')
+echo "PUBLIC_IP=$(curl -s ifconfig.me | awk '{print $1}')" > /home/ec2-user/Tic-tac-toe/.env
 git clone https://github.com/Pawel-Kluska/Tic-tac-toe.git /home/ec2-user/Tic-tac-toe
 sudo docker-compose -f /home/ec2-user/Tic-tac-toe/docker-compose-prod.yml up -d
-
 #sudo apt update -y
 #sudo apt install apache2 -y
 #sudo systemctl start apache2
